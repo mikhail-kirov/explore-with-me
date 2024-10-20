112dd53d3a42b01db8c545c05472062fbd3fe58a
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-10-20 12:57:26 +0300

    stats_v2.2

--- statistics/server/src/main/java/ru/practicum/service/HitsService.java
+++ statistics/server/src/main/java/ru/practicum/service/HitsService.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 import java.util.List;
 
 public interface HitsService {
+
     EndpointHitDto createHits(EndpointHitDto hit);
+
     Collection<ViewStatsDto> getStats(List<String> uris, String start, String end, boolean unique);
 }
