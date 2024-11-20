47c2aa5951630dc883976d491cdbc037204c2b33
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-11-20 22:10:54 +0300

    main_svc_1.01

--- statistics/dto/src/main/java/ru/practicum/model/ViewStatsDto.java
+++ statistics/dto/src/main/java/ru/practicum/model/ViewStatsDto.java
@@ -9,13 +9,9 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Builder
 public class ViewStatsDto implements Serializable {
-
     private String app;
-
     private String uri;
-
     private Long hits;
-
     private String ip;
 
     public ViewStatsDto(String app, String uri, Long hits, String ip) {
